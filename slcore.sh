266ad520f0f4966acbea2cd950ef8651949c29c5
Prabhu Subramanian <prabhu@shiftleft.io>
2022-09-13 12:27:17 +0000

    Improvements

---
+++ slcore.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+mvn compile package -Dmaven.test.skip=true
+sl analyze --wait --app vuln-spring --tag branch=$GIT_BRANCH --java --cpg target/vuln-spring-0.0.1-SNAPSHOT.jar
+# Compare the current scan against master branch
+#sl check-analysis --v2 --app vuln-spring --source tag.branch=master --target tag.branch=$GIT_BRANCH --report
+# Display summary from last scan
+sl check-analysis --v2 --app vuln-spring --report
+# Compare the current branch against the last scan from the same branch
+# sl check-analysis --v2 --app vuln-spring --branch=$GIT_BRANCH --report
